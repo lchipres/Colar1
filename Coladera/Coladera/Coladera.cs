375c8f9fda65f5eddb72047321f7bb8e130e8845
Leonardo Daniel Chipres Arteaga <33239864+lchipres@users.noreply.github.com>
2018-03-22 21:08:14 -0600

    Add files via upload

---
+++ Coladera/Coladera/Coladera.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coladera
+{
+    class Coladera
+    {
+        protected static int _num;
+        protected bool[] _cousins= new bool[1000];
+
+        public Coladera(int num)
+        {
+            _num = num;
+        }
+
+        public void llenado()
+        {
+            for(int i = 0; i < _num; i++)
+            {
+                _cousins[i] = true;
+            }
+        }
+
+        public void colado()
+        {
+            for(int j = 2; j < _num; j++)
+            {
+                for(int i = 2; i * j < _num; i++)
+                {
+                    _cousins[i * j] = false;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string s="";
+            for(int i=2;i<_num;i++)
+            {
+                if (_cousins[i])
+                {
+                    s += i.ToString() + ", ";
+                }
+            }
+            return s;
+        }
+    }
+}
---
+++ Coladera/Coladera/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coladera
+{
+    public partial class Form1 : Form
+    {
+        Coladera c;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            c = new Coladera(1000);
+            c.llenado();
+            c.colado();
+            textBox2.Text = c.ToString();
+        }
+    }
+}
